dc06c24c3e7a58791b6f8986112bb4513bb7610d
Tyssan <tbrunier@student.42perpignan.fr>
2024-09-07 12:15:09 +0200

    ft_list_size

---
+++ exam_rank_2/level_3/ft_list_size/ft_list_size.c
@@ -0,0 +1,38 @@
+#include "ft_list.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
+
+	i = 0;
+	while (begin_list)
+	{
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (i);
+}
+
+t_list *create_node(void *data)
+{
+	t_list *node = (t_list *)malloc(sizeof(t_list));
+	node->data = data;
+	node->next = NULL;
+}
+
+int	main(void)
+{
+	t_list *node1 = create_node("1");
+	t_list *node2 = create_node("2");
+	t_list *node3 = create_node("3");
+	t_list *node4 = create_node("3");
+
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = node4;
+	node4->next = NULL;
+	int len = ft_list_size(node1);
+	printf("%d\n", len);
+}
